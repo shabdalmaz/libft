72e040a11d4b4f671cdf6f8be31fe8b55d86459a
shabdalmaz <ashabdan@student.42.us.org>
2020-05-07 20:17:47 -0700

    renamed macro

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,6 +1,6 @@
 #include <string.h>
 
-#define BUF_SIZE sizeof(unsigned int)
+#define SUBSTR_SIZE sizeof(unsigned int)
 typedef	unsigned int substr;
 
 void	*ft_memcpy(void *dst0, const void *src0, size_t n)
@@ -11,16 +11,16 @@ void	*ft_memcpy(void *dst0, const void *src0, size_t n)
 
 	if (n <= 0 || dst0 == src0)
 		return (dst0);
-	i = n / BUF_SIZE;
+	i = n / SUBSTR_SIZE;
 	dst = (char *)dst0;
 	src = (const char *)src0;
 	while (i--)
 	{
 		*(substr *)dst = *(substr *)src;
-		dst += BUF_SIZE;
-		src += BUF_SIZE;
+		dst += SUBSTR_SIZE;
+		src += SUBSTR_SIZE;
 	}
-	i = n % BUF_SIZE;
+	i = n % SUBSTR_SIZE;
 	while (i--)
 	{
 		*dst = *src;
