7370c347721a55fd4ade257eb9987cd9fb5382cd
shabdalmaz <ashabdan@student.42.us.org>
2020-06-04 03:45:04 -0700

    need to learn that magic line with bitwise condition

---
+++ ft_memchr.c
@@ -0,0 +1,33 @@
+#include <string.h>
+
+#define WORD_SIZE sizeof(unsigned int)
+#define FST_BIT 0x01010101
+#define LST_BIT 0x80808080
+typedef unsigned int word;
+
+void	ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char	*c_ptr;
+	unsigned char		chr;
+	word			mask;
+
+	c_ptr = (unsigned char *)s;
+	chr = (unsigned char)c;
+	if (n > WORD_SIZE)
+	{
+		mask = (word)chr;
+		mask = mask | (mask << 8);
+		mask = mask | (mask << 16);
+		while (n >= WORD_SIZE)
+		{
+			if ((((*(word *)c_ptr ^ mask) - FST_BIT) \
+			& ~(*(word *)c_ptr ^ mask) & LST_BIT))
+				break ;
+			n -= WORD_SIZE;
+			c_ptr += WORD_SIZE;
+		}
+	}
+	while (n-- && *c_ptr == chr)
+		c_ptr++;
+	return (n ? (void *)c_ptr : NULL);
+}
