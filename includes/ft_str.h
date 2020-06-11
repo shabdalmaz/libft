63845da6250d8c75bd6ce75f05d8720aa560f05d
shabdalmaz <ashabdan@student.42.us.org>
2020-06-11 00:37:18 -0700

    string functions

--- includes/ft_str.h
+++ includes/ft_str.h
@@ -8,5 +8,8 @@ typedef unsigned char	byte;
 size_t			ft_strlen(const char *s);
 size_t			ft_strlcpy(char *dst, const char *src, size_t dst_size);
 size_t			ft_strlcat(char *dst, const char *src, size_t dst_size);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strrchr(const char *s, int c);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
