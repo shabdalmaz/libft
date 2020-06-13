26885ed819598c9fbb7e4fef057da91712b6716e
shabdalmaz <ashabdan@student.42.us.org>
2020-06-12 23:09:51 -0700

    ft_ctype.h

---
+++ includes/ft_ctype.h
@@ -0,0 +1,16 @@
+#ifndef FT_CTYPE_H
+# define FT_CTYPE_H
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+int	ft_islower(int c);
+int	ft_isupper(int c);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+int	ft_isxdigit(int c);
+int	ft_isspace(int c);
+
+#endif
