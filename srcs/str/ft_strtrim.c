8b5efd364f20030d04d2a5142e7b530ac4a59525
shabdalmaz <ashabdan@student.42.us.org>
2020-06-24 18:24:50 -0700

    ft_strchr used instead of ft_memchr

--- srcs/str/ft_strtrim.c
+++ srcs/str/ft_strtrim.c
@@ -1,5 +1,4 @@
 #include "ft_str.h"
-#include "ft_mem.h"
 
 char	*ft_strtrim(const char *s, const char *set)
 {
@@ -7,13 +6,15 @@ char	*ft_strtrim(const char *s, const char *set)
 	size_t	set_len;
 	size_t	s_len;
 
+	if (!(s && set))
+		return (NULL);
 	ret = NULL;
 	set_len = ft_strlen(set);
 	s_len = 0;
-	while (ft_memchr(set, *s, set_len))
+	while (*s && ft_strchr(set, *s))
 		s += 1;
-	s_len = ft_strlen(s) - 1;
-	while (ft_memchr(set, *(s + len), set_len))
+	s_len = ft_strlen(s);
+	while (s_len && ft_strchr(set, *(s + len - 1)))
 		s_len -= 1;
 	ret = ft_substr(s, 0, s_len);
 	return (ret);
