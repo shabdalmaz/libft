0c69d03df934f489dbe8b771558337b8891f171e
shabdalmaz <ashabdan@student.42.us.org>
2020-06-24 19:03:49 -0700

    ft_substrcnt is uploaded

--- includes/ft_str.h
+++ includes/ft_str.h
@@ -16,5 +16,6 @@ char			*ft_strdup(const char *s);
 char			*ft_substr(const char *s, unsigned int start, size_t len);
 char			*ft_strjoin(const char *s1, const char *s2);
 char			*ft_strtrim(const char *s, const char *set);
+size_t			*ft_substrcnt(const char *s, char delim);
 
 #endif
