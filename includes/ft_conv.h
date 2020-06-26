fc91941053e9cb457d4287ea3f912d4d4768d5a3
shabdalmaz <ashabdan@student.42.us.org>
2020-06-26 01:35:04 -0700

    ft_itoa is uploaded

--- includes/ft_conv.h
+++ includes/ft_conv.h
@@ -2,5 +2,6 @@
 # define FT_CONV_H
 
 int	ft_atoi(const char *nptr);
+char	*ft_itoa(int n);
 
 #endif
---
+++ srcs/conv/ft_itoa.c
@@ -0,0 +1,27 @@
+#include "ft_mem.h"
+#include "ft_str.h"
+#include "ft_count.h"
+
+char	*ft_itoa(int n)
+{
+	char	*ret;
+	size_t	len;
+	int	sign;
+
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	sign = (n < 0) : 1 : 0;
+	len = ft_cntdig(n, 10) + sign;
+	if (!(ret = (char *)calloc(len + 1, sizeof(char))))
+		return (NULL);
+	n *= (sign) ? -1 : 1;
+	while (len)
+	{
+		*(ret + len - 1) = (n % 10) + '0';
+		n /= 10;
+		len -= 1;
+	}
+	if (sign)
+		*(ret + len) = '-';
+	return (ret);
+}
