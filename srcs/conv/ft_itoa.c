1d34874dcadb8173f83ce084ebb2398bc255169c
shabdalmaz <ashabdan@student.42.us.org>
2020-06-29 13:45:20 -0700

    fixed ft_itoa

--- srcs/conv/ft_itoa.c
+++ srcs/conv/ft_itoa.c
@@ -10,9 +10,9 @@ char	*ft_itoa(int n)
 
 	if (n == -2147483648)
 		return (ft_strdup("-2147483648"));
-	sign = (n < 0) : 1 : 0;
+	sign = (n < 0) ? 1 : 0;
 	len = ft_cntdig(n, 10) + sign;
-	if (!(ret = (char *)calloc(len + 1, sizeof(char))))
+	if (!(ret = (char *)ft_calloc(len + 1, sizeof(char))))
 		return (NULL);
 	n *= (sign) ? -1 : 1;
 	while (len)
