b8196b2e55bb765ea3d4983b76c62fbb3d605b29
shabdalmaz <ashabdan@student.42.us.org>
2020-06-30 00:16:57 -0700

    ft_putnbr_fd without memory allocation

--- srcs/io/ft_putnbr_fd.c
+++ srcs/io/ft_putnbr_fd.c
@@ -4,12 +4,15 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*n_str;
-
-	if ((n_str = ft_itoa(n)))
+	if (n == -2147483648)
+		return (ft_putstr_fd("-2147483648", fd));
+	(n < 0) ? ft_putchar_fd('-', fd) : 0;
+	n = (n < 0) ? -n : n;
+	if (n < 10)
+		ft_putchar_fd('0' + n, fd);
+	else
 	{
-		ft_putstr_fd(n_str, fd);
-		free(n_str);
-		n_str = NULL;
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
 	}
 }
