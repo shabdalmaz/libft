d92b4f000cbb467a22698dd7dd4ca9db6bde7383
shabdalmaz <ashabdan@student.42.us.org>
2020-07-01 00:09:21 -0700

    ft_lstnew without free()

--- srcs/lst/ft_lstnew.c
+++ srcs/lst/ft_lstnew.c
@@ -11,12 +11,10 @@ t_list	*ft_lstnew(void *content)
 	if (!(content && node))
 		return (NULL);
 	content_size = ft_strlen(content) * sizeof(void);
-	if (!(node->content = malloc(content_size)))
-	{
-		free(node);
-		return (NULL);
-	}
-	ft_memcpy(node->content, content, content_size);
+	if ((node->content = malloc(content_size)))
+		ft_memcpy(node->content, content, content_size);
+	else
+		node->content = NULL;
 	node->next = NULL;
 	return (node);
 }
