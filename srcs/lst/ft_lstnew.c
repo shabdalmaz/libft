df0efc02c76756ff8ff43dda0f2095437b73273b
shabdalmaz <ashabdan@student.42.us.org>
2020-07-02 15:28:41 -0700

    new subject ft_lstnew

--- srcs/lst/ft_lstnew.c
+++ srcs/lst/ft_lstnew.c
@@ -1,20 +1,12 @@
 #include "ft_lst.h"
-#include "ft_str.h"
-#include "ft_mem.h"
 
 t_list	*ft_lstnew(void *content)
 {
 	t_list	*node;
-	size_t	content_size;
 
-	node = (t_list *)malloc(sizeof(t_list));
-	if (!(content && node))
+	if (!(node = (t_list *)malloc(sizeof(t_list))))
 		return (NULL);
-	content_size = ft_strlen(content) * sizeof(void);
-	if ((node->content = malloc(content_size)))
-		ft_memcpy(node->content, content, content_size);
-	else
-		node->content = NULL;
+	node->content = content;
 	node->next = NULL;
 	return (node);
 }
